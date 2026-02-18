5b6d2a3282c24cf7835084f6973d3002939c41b4
mrwrench24 <jake.squatrito@icloud.com>
2026-02-18 14:06:30 -0500

    updated client component demo

--- app/labs/lab3/ClientComponentDemo.tsx
+++ app/labs/lab3/ClientComponentDemo.tsx
@@ -1,25 +1,14 @@
 "use client";
+import { usePathname } from "next/navigation";
 
 export default function ClientComponentDemo() {
   // This runs ONLY in the browser
-  alert(
-    "Hello! This alert runs in the browser because this is a Client Component.",
-  );
+  const pathname = usePathname();
 
   return (
     <div>
       <h1>Client Component Demo</h1>
-      <p>You should have seen an alert pop up when the page loaded.</p>
-      <p>
-        {" "}
-        <code>alert()</code> is a browser API — it only exists in the browser,
-        so this code can <strong>only run on the client</strong>.{" "}
-      </p>
-      <p>
-        {" "}
-        If you removed <code>'use client'</code>, the build would fail because
-        <code>alert</code> is undefined on the server.{" "}
-      </p>
+      <p>Current pathname: {pathname}</p>
     </div>
   );
 }
