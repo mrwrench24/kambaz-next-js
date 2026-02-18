da5a3989ecf0607fd692e598be3572aa3ee1e54d
mrwrench24 <jake.squatrito@icloud.com>
2026-02-18 14:02:44 -0500

    strict

--- app/(kambaz)/courses/[cid]/Breadcrumb.tsx
+++ app/(kambaz)/courses/[cid]/Breadcrumb.tsx
@@ -8,11 +8,13 @@ export default function Breadcrumb({
 }) {
   const pathname = usePathname();
   const lastPart = pathname.split("/").pop();
+  const label = lastPart
+    ? lastPart.slice(0, 1).toUpperCase() + lastPart.slice(1)
+    : "";
 
   return (
     <span>
-      {course?.name} &gt;{" "}
-      {lastPart?.slice(0, 1).toUpperCase() + lastPart?.slice(1)}
+      {course?.name} &gt; {label}
     </span>
   );
 }
