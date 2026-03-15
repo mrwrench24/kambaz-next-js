e31c55e836e56b0e40041815bdb36e428de40c41
mrwrench24 <jake.squatrito@icloud.com>
2026-03-15 14:16:34 -0400

    types

--- app/(kambaz)/courses/[cid]/assignments/AssignmentControlButtons.tsx
+++ app/(kambaz)/courses/[cid]/assignments/AssignmentControlButtons.tsx
@@ -1,7 +1,11 @@
 import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "../modules/GreenCheckmark";
 import { FaTrash } from "react-icons/fa6";
-export default function AssignmentControlButtons({ handleClick }) {
+export default function AssignmentControlButtons({
+  handleClick,
+}: {
+  handleClick: () => void;
+}) {
   return (
     <div className="float-end">
       <FaTrash onClick={handleClick} color="red" className="fs-4 m-2" />
