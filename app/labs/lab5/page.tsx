a64072b1dd94444ed494c51f94bdc230f00ce72f
mrwrench24 <jake.squatrito@icloud.com>
2026-03-18 10:11:55 -0400

    Up to 5.2.6

--- app/labs/lab5/page.tsx
+++ app/labs/lab5/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import EnvironmentVariables from "./EnvironmentVariables";
+import HttpClient from "./HttpClient";
 import Module from "./Module";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 
 export default function Lab5() {
@@ -29,6 +32,12 @@ export default function Lab5() {
       <Module />
 
       <WorkingWithArrays />
+
+      <HttpClient />
+
+      <WorkingWithObjectsAsynchronously />
+
+      <WorkingWithArraysAsynchronously />
     </div>
   );
 }
