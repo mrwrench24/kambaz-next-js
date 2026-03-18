bf50ab33288552372356584d5a3ecccc18f3d390
mrwrench24 <jake.squatrito@icloud.com>
2026-03-18 15:53:43 -0400

    lab

--- app/labs/lab5/WorkingWithArraysAsynchronously.tsx
+++ app/labs/lab5/WorkingWithArraysAsynchronously.tsx
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from "react";
 import * as client from "./client";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
-import { FaTrash } from "react-icons/fa6";
+import { FormControl, ListGroup, ListGroupItem } from "react-bootstrap";
+import { FaPencil, FaTrash } from "react-icons/fa6";
 import { FaPlusCircle } from "react-icons/fa";
+import { TiDelete } from "react-icons/ti";
+
 export default function WorkingWithArraysAsynchronously() {
   const [todos, setTodos] = useState<any[]>([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const editTodo = (todo: any) => {
+    const updatedTodos = todos.map((t) =>
+      t.id === todo.id ? { ...todo, editing: true } : t,
+    );
+    setTodos(updatedTodos);
+  };
+
+  const updateTodo = async (todo: any) => {
+    try {
+      await client.updateTodo(todo);
+      setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
+    } catch (error: any) {
+      setErrorMessage(error.response.data.message);
+    }
+  };
 
   const fetchTodos = async () => {
     const todos = await client.fetchTodos();
@@ -21,6 +40,25 @@ export default function WorkingWithArraysAsynchronously() {
     setTodos(todos);
   };
 
+  const postNewTodo = async () => {
+    const newTodo = await client.postNewTodo({
+      title: "New Posted Todo",
+      completed: false,
+    });
+    setTodos([...todos, newTodo]);
+  };
+
+  const deleteTodo = async (todo: any) => {
+    try {
+      await client.deleteTodo(todo);
+      const newTodos = todos.filter((t) => t.id !== todo.id);
+      setTodos(newTodos);
+    } catch (error: any) {
+      console.log(error);
+      setErrorMessage(error.response.data.message);
+    }
+  };
+
   useEffect(() => {
     fetchTodos();
   }, []);
@@ -28,31 +66,69 @@ export default function WorkingWithArraysAsynchronously() {
   return (
     <div id="wd-asynchronous-arrays">
       <h3>Working with Arrays Asynchronously</h3>
+      {errorMessage && (
+        <div
+          id="wd-todo-error-message"
+          className="alert alert-danger mb-2 mt-2"
+        >
+          {errorMessage}
+        </div>
+      )}
       <h4>Todos</h4>
       <FaPlusCircle
         onClick={createNewTodo}
         className="text-success float-end fs-3"
       />
+      <FaPlusCircle
+        onClick={postNewTodo}
+        className="text-primary float-end fs-3 me-3"
+        id="wd-post-todo"
+      />
       <ListGroup>
         {todos.map((todo) => (
           <ListGroupItem key={todo.id}>
+            <FaPencil
+              onClick={() => editTodo(todo)}
+              className="text-primary float-end me-2 mt-1"
+            />
+            <input
+              type="checkbox"
+              defaultChecked={todo.completed}
+              className="form-check-input me-2 float-start"
+              onChange={(e) =>
+                updateTodo({ ...todo, completed: e.target.checked })
+              }
+            />
+            {!todo.editing ? (
+              <span
+                style={{
+                  textDecoration: todo.completed ? "line-through" : "none",
+                }}
+              >
+                {todo.title}
+              </span>
+            ) : (
+              <FormControl
+                className="w-50 float-start"
+                defaultValue={todo.title}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    updateTodo({ ...todo, editing: false });
+                  }
+                }}
+                onChange={(e) => updateTodo({ ...todo, title: e.target.value })}
+              />
+            )}
             <FaTrash
               onClick={() => removeTodo(todo)}
               className="text-danger float-end mt-1"
               id="wd-remove-todo"
             />
-            <input
-              type="checkbox"
-              className="form-check-input me-2"
-              defaultChecked={todo.completed}
+            <TiDelete
+              onClick={() => deleteTodo(todo)}
+              className="text-danger float-end me-2 fs-3"
+              id="wd-delete-todo"
             />
-            <span
-              style={{
-                textDecoration: todo.completed ? "line-through" : "none",
-              }}
-            >
-              {todo.title}{" "}
-            </span>
           </ListGroupItem>
         ))}
       </ListGroup>{" "}
--- app/labs/lab5/client.ts
+++ app/labs/lab5/client.ts
@@ -30,3 +30,18 @@ export const createNewTodo = async () => {
   const response = await axios.get(`${TODOS_API}/create`);
   return response.data;
 };
+
+export const postNewTodo = async (todo: any) => {
+  const response = await axios.post(`${TODOS_API}`, todo);
+  return response.data;
+};
+
+export const deleteTodo = async (todo: any) => {
+  const response = await axios.delete(`${TODOS_API}/${todo.id}`);
+  return response.data;
+};
+
+export const updateTodo = async (todo: any) => {
+  const response = await axios.put(`${TODOS_API}/${todo.id}`, todo);
+  return response.data;
+};
