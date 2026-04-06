12b5628d657e2c79c558057548d4d4ae77781c1b
mrwrench24 <jake.squatrito@icloud.com>
2026-04-06 19:51:43 -0400

    extra store prevents errors?

--- app/(kambaz)/account/Layout.tsx
+++ app/(kambaz)/account/Layout.tsx
@@ -1,23 +1,28 @@
 "use client";
 import { ReactNode } from "react";
 import AccountNavigation from "./Navigation";
+import { Provider } from "react-redux";
+import store from "../store";
+
 export default function AccountLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
-    <div id="wd-kambaz">
-      <table>
-        <tbody>
-          <tr>
-            <td valign="top">
-              <AccountNavigation />
-            </td>
-            <td valign="top" width="100%">
-              {children}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <Provider store={store}>
+      <div id="wd-kambaz">
+        <table>
+          <tbody>
+            <tr>
+              <td valign="top">
+                <AccountNavigation />
+              </td>
+              <td valign="top" width="100%">
+                {children}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Provider>
   );
 }
