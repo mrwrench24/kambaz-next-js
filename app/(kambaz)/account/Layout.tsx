1ccf2f92212af4c691ddc48aac099d02e00840ab
mrwrench24 <jake.squatrito@icloud.com>
2026-04-06 19:58:19 -0400

    duplicate iD maybe ?

--- app/(kambaz)/account/Layout.tsx
+++ app/(kambaz)/account/Layout.tsx
@@ -1,28 +1,23 @@
 "use client";
 import { ReactNode } from "react";
 import AccountNavigation from "./Navigation";
-import { Provider } from "react-redux";
-import store from "../store";
-
 export default function AccountLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
-    <Provider store={store}>
-      <div id="wd-kambaz">
-        <table>
-          <tbody>
-            <tr>
-              <td valign="top">
-                <AccountNavigation />
-              </td>
-              <td valign="top" width="100%">
-                {children}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </Provider>
+    <div id="wd-kambaz-account-layout">
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top">
+              <AccountNavigation />
+            </td>
+            <td valign="top" width="100%">
+              {children}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
