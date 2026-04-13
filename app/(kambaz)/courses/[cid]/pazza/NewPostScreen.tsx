fd318697c10dad3e7b6a5dc8d3573e97c2b76362
mrwrench24 <jake.squatrito@icloud.com>
2026-04-13 18:17:45 -0400

    stateful new post screen / more interactive

--- app/(kambaz)/courses/[cid]/pazza/NewPostScreen.tsx
+++ app/(kambaz)/courses/[cid]/pazza/NewPostScreen.tsx
@@ -11,32 +11,69 @@ import {
 } from "react-simple-wysiwyg";
 import { usePazzaContext } from "./PazzaContext";
 import folders from "./database/folders";
+import { Folder } from "./types/types";
 
 export default function NewPostScreen() {
-  const [value, setValue] = useState("simple text");
   const { setPage } = usePazzaContext();
 
-  const [selected, setSelected] = useState<string[]>([]);
+  const [newPost, setNewPost] = useState<{
+    postType: "question" | "note";
+    postTo: "all" | "individual";
+    // folder IDs
+    folders: string[];
+    summary: string;
+    details: string;
+  }>({
+    postType: "question",
+    postTo: "all",
+    folders: [],
+    summary: "",
+    details: "",
+  });
 
   return (
     <div>
       <div className="p-2 d-flex align-items-center" id="post-types">
         Post Type
         <span className="ps-4">
-          <input type="radio" /> Question
+          <input
+            name="postType"
+            type="radio"
+            checked={newPost.postType === "question"}
+            onChange={() => setNewPost({ ...newPost, postType: "question" })}
+          />{" "}
+          Question
         </span>
         <span className="ps-4">
-          <input type="radio" /> Note
+          <input
+            name="postType"
+            type="radio"
+            checked={newPost.postType === "note"}
+            onChange={() => setNewPost({ ...newPost, postType: "note" })}
+          />{" "}
+          Note
         </span>
       </div>
       <div className="p-2 light-blue-bg" id="post-create">
         <div className="pt-2">
           Post To
           <span className="ps-4">
-            <input type="radio" /> Entire Class
+            <input
+              name="postTo"
+              type="radio"
+              checked={newPost.postTo === "all"}
+              onChange={() => setNewPost({ ...newPost, postTo: "all" })}
+            />{" "}
+            Entire Class
           </span>
           <span className="ps-4">
-            <input type="radio" /> Individual Student(s) / Instructors
+            <input
+              name="postTo"
+              type="radio"
+              checked={newPost.postTo === "individual"}
+              onChange={() => setNewPost({ ...newPost, postTo: "individual" })}
+            />{" "}
+            Individual Student(s) / Instructors
           </span>
         </div>
 
@@ -44,7 +81,7 @@ export default function NewPostScreen() {
           Select Folders
           <div className="d-flex flex-wrap gap-2 pt-1">
             {folders.map((folder) => {
-              const isSelected = selected.includes(folder.id);
+              const isSelected = newPost.folders.includes(folder.id);
 
               return (
                 <span
@@ -53,9 +90,15 @@ export default function NewPostScreen() {
                   role="button"
                   onClick={() => {
                     if (isSelected) {
-                      setSelected(selected.filter((fid) => fid !== folder.id));
+                      setNewPost({
+                        ...newPost,
+                        folders: newPost.folders.filter((f) => f !== folder.id),
+                      });
                     } else {
-                      setSelected([...selected, folder.id]);
+                      setNewPost({
+                        ...newPost,
+                        folders: [...newPost.folders, folder.id],
+                      });
                     }
                   }}
                 >
@@ -75,14 +118,24 @@ export default function NewPostScreen() {
 
         <div className="d-flex align-items-center pt-3">
           Summary
-          <input className="ms-2 flex-fill form-control" type="text" />
+          <input
+            className="ms-2 flex-fill form-control"
+            type="text"
+            placeholder="A brief summary here..."
+          />
         </div>
 
         <div className="d-flex align-items-center pt-3">
           Details
           <div className="bg-white flex-fill m-2">
             <EditorProvider>
-              <Editor value={value}>
+              <Editor
+                value={newPost.details}
+                onChange={(e) =>
+                  setNewPost({ ...newPost, details: e.target.value })
+                }
+                placeholder="Your question / note here..."
+              >
                 <Toolbar>
                   <BtnBold />
                   <BtnItalic />
@@ -94,8 +147,12 @@ export default function NewPostScreen() {
         </div>
 
         <span className="ms-5 pt-2">
-          <Button className="p-2 m-2">Post My Question</Button>
-          <Button className="p-2 m-2">Cancel</Button>
+          <Button className="p-2 m-2" onClick={() => setPage("cag")}>
+            Post My Question
+          </Button>
+          <Button className="p-2 m-2" onClick={() => setPage("cag")}>
+            Cancel
+          </Button>
         </span>
       </div>
     </div>
