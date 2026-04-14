b62d6c2d49db423313dd1ae5ed7469aea2f29cdf
mrwrench24 <jake.squatrito@icloud.com>
2026-04-14 17:54:00 -0400

    small things from the rubric i added

--- app/(kambaz)/courses/[cid]/pazza/Navigation.tsx
+++ app/(kambaz)/courses/[cid]/pazza/Navigation.tsx
@@ -11,13 +11,27 @@ export default function PazzaNavigation({ cid }: { cid: string }) {
     (state: RootState) => state.accountReducer,
   );
 
+  const { courses } = useSelector((state: RootState) => state.coursesReducer);
+
+  const course = courses.find((c) => c._id === cid);
+
+  console.log(course);
+
   return (
     <div className="bg-primary text-white p-1 d-flex align-items-center">
-      <span className="fw-bold fs-4 p-2">Pazza</span>
+      <span
+        className="fw-bold fs-4 p-2"
+        role="button"
+        onClick={() => setPage("cag")}
+      >
+        Pazza
+      </span>
+
+      <span className="fw-bold fs-5 ps-4">
+        {course.number}: {course.name}
+      </span>
 
       <div className="ms-auto d-flex align-items-center me-3">
-        <span className="fw-bold fs-5 p-2">CS 4550</span>
-
         <span
           className={`p-2 ${page !== "manage_class" && "nav-link-selected"}`}
           role="button"
