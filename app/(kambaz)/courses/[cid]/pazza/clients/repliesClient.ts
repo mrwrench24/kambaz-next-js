262fd4a20d7c2eb2aeabf9084ac7effab8a1fc52
mrwrench24 <jake.squatrito@icloud.com>
2026-04-16 13:41:53 -0400

    fixing up, deletion

--- app/(kambaz)/courses/[cid]/pazza/clients/repliesClient.ts
+++ app/(kambaz)/courses/[cid]/pazza/clients/repliesClient.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { FollowupReply } from "../types/types";
-import { FOLDERS_API } from "./foldersClient";
 const axiosWithCredentials = axios.create({ withCredentials: true });
 
 export const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
@@ -12,7 +11,7 @@ export const getReplyByIds = async (ids: string[]) => {
 };
 
 export const createReply = async (cid: string, authorId: string) => {
-  const response = await axiosWithCredentials.post(`${FOLDERS_API}`, {
+  const response = await axiosWithCredentials.post(`${REPLIES_API}`, {
     cid,
     authorId,
   });
@@ -21,14 +20,11 @@ export const createReply = async (cid: string, authorId: string) => {
 };
 
 export const updateReply = async (reply: FollowupReply) => {
-  const response = await axiosWithCredentials.put(`${FOLDERS_API}`, reply);
+  const response = await axiosWithCredentials.put(`${REPLIES_API}`, reply);
   return response.data;
 };
 
 export const deleteReply = async (rid: string) => {
-  const response = await axiosWithCredentials.delete(`${FOLDERS_API}`, {
-    data: rid,
-  });
-
+  const response = await axiosWithCredentials.delete(`${REPLIES_API}/${rid}`);
   return response.data;
 };
